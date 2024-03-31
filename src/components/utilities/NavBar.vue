52a96551213b3d6cf86342430b1a511f6b30b291
Delsus78 <delsustheugm@gmail.com>
2024-03-31 05:24:08 +0200

    version oups

--- src/components/utilities/NavBar.vue
+++ src/components/utilities/NavBar.vue
@@ -220,7 +220,7 @@ const isChristmas = computed(() => {
         </div>
     </nav>
     <div class="version">
-        <span class="version-text prevent-select">Version 2.3</span>
+        <span class="version-text prevent-select">Version 2.4</span>
     </div>
 </template>
 <style scoped>
